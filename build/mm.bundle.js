b6a3d1b286fd579cffe285ef49ce60544b6225a8
luis fernando lopez mariz <ll57440@gmail.com>
2016-04-10 00:16:58 -0500

    automatisation de acceso y traducción a Comunis

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -21403,7 +21403,7 @@ angular.module('mm.core')
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch","en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
+    "demo_sites": {"COMUNIS": {"url": "http://www.comunis.org/ecomunis", "username": "", "password": ""}, "demo": {"url": "http://fluxusmx.dnsalias.com/ecomunis", "username": "admin", "password": "Comunis2016."}, "comunis": {"url": "http://www.comunis.org/ecomunis", "username": "", "password": ""}},
     "gcmpn": "694767596569",
     "customurlscheme": "comunismobile"
 }
