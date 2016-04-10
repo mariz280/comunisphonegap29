09efc81166f4ba53fea4c5a719bb9ae2cde32c3a
luis fernando lopez mariz <ll57440@gmail.com>
2016-04-10 00:45:26 -0500

    cambio es-mx to mx

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -21399,11 +21399,11 @@ angular.module('mm.core')
     "versioncode" : "2010",
     "versionname" : "2.9",
     "cache_expiration_time" : 300000,
-    "default_lang" : "es-mx",
-    "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch","en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
+    "default_lang" : "mx",
+    "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch","en": "English", "es": "Español", "mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "demo_sites": {"COMUNIS": {"url": "http://www.comunis.org/ecomunis", "username": "", "password": ""}, "demo": {"url": "http://fluxusmx.dnsalias.com/ecomunis", "username": "admin", "password": "Comunis2016."}, "comunis": {"url": "http://www.comunis.org/ecomunis", "username": "", "password": ""}},
+    "demo_sites": {"COMUNIS": {"url": "http://www.comunis.org/ecomunis", "username": "Su Usuario Por Favor", "password": ""}, "demo": {"url": "http://fluxusmx.dnsalias.com/ecomunis", "username": "admin", "password": "Comunis2016."}, "comunis": {"url": "http://www.comunis.org/ecomunis", "username": "", "password": ""}},
     "gcmpn": "694767596569",
     "customurlscheme": "comunismobile"
 }
